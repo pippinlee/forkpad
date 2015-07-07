d512241fbc822ba60a805d24e78fb1e25cfd9eb4
Pippin Lee <pippinblee@gmail.com>
2015-07-07 11:03:17 -0400

    small test fixes

--- test/test.js
+++ test/test.js
@@ -1,34 +1,30 @@
-
-
 var assert = require('assert');
 var superagent = require('superagent');
 var server = require('../server.js');
 var status = require('http-status');
 var http = require('http');
 
- var testingID = 0;
+var testingID = 0;
+var app = -1;
 
-
-  var app = -1;
+// test 1 and 2
 describe('/api/pad/', function() {
 
   before(function() {
     app = server(3000);
   });
- 
 
- 
   it('fetches wattpad story', function(done) {
     superagent.get('http://localhost:3000/api/pad/30606794').end(function(err, res) {
       assert.ifError(err);
       assert.equal(res.status, status.OK);
       // console.log(res.text);
-      assert(res.text.length != 0);
+      assert(res.text.length !== 0);
       done();
     });
   });
 
-   it('fails gracefully when wattpad part does\'t exist', function(done) {
+  it('fails gracefully when wattpad part does\'t exist', function(done) {
     superagent.get('http://localhost:3000/api/pad/404').end(function(err, res) {
       assert.ifError(err);
       assert.equal(res.status, status.OK);
@@ -36,20 +32,16 @@ describe('/api/pad/', function() {
       done();
     });
   });
-
-
-
-
 });
 
+// test 3
 describe('/api/insert/', function() {
 
-before(function( done ){
-    checkapp( done );
+  before(function( done ){
+      checkapp( done );
   });
 
-
-it('inserts text into mongo (make sure mongod is running, hacky for now)', function(done) {
+  it('inserts text into mongo (make sure mongod is running, hacky for now)', function(done) {
     superagent.get('http://localhost:3000/api/insert/testtext').end(function(err, res) {
       assert.ifError(err);
       assert.equal(res.status, status.OK);
@@ -60,33 +52,41 @@ it('inserts text into mongo (make sure mongod is running, hacky for now)', funct
     });
   });
 
-
-var checkapp = function(done) {
-  // console.log(testingID);
-    if (app!= -1) done();
-    else setTimeout( function(){ checkapp(done) }, 500 );
-  }
+  var checkapp = function(done) {
+    // console.log(testingID);
+    if (app !== -1){
+      done();
+    }
+    else setTimeout( function(){
+      checkapp(done);
+    }, 500 );
+  };
 });
 
+// test 4
 describe('/api/retrieve/', function() {
 
-before(function( done ){
-    checkinsert( done );
+  before(function( done ){
+      checkinsert( done );
   });
 
-  it('retrieves that text from mongo', function(done){ 
+  it('retrieves that text from mongo', function(done){
     // Only gets here once `someCondition` is satisfied
-  superagent.get('http://localhost:3000/api/retrieve/'+testingID.replace(/['"]+/g, '')).end(function(err, res) {
+    superagent.get('http://localhost:3000/api/retrieve/'+testingID.replace(/['"]+/g, '')).end(function(err, res) {
       assert.ifError(err);
       assert.equal(res.status, status.OK);
-      assert(res.text == "testtext");
+      assert(res.text === "testtext");
       done();
     });
   });
 
-var checkinsert = function(done) {
-	// console.log(testingID);
-    if (testingID!=0) done();
-    else setTimeout( function(){ check(done) }, 500 );
-  }
+  var checkinsert = function(done) {
+  	// console.log(testingID);
+      if (testingID !== 0){
+        done();
+      }
+      else setTimeout(function(){
+        checkinsert(done);
+      }, 500 );
+    };
 });
